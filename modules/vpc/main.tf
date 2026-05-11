caccd38cd44aa75c4861809155bf22df62308d4b
shiva1925 <shivasai1925@gmai.com>
2026-05-12 00:21:59 +0530

    Added NAT gateway and private networking

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -55,4 +55,47 @@ resource "aws_route_table_association" "public_assoc" {
   route_table_id = aws_route_table.public_rt.id
 }
 
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
 
+  tags = {
+    Name = "nat-eip"
+  }
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet_1.id
+
+  tags = {
+    Name = "nat-gateway"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_route_table" "private_rt" {
+
+  vpc_id = aws_vpc.main.id
+
+  route {
+
+    cidr_block     = "0.0.0.0/0"
+
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+
+  }
+
+  tags = {
+
+    Name = "private-route-table"
+
+  }
+
+}
+
+resource "aws_route_table_association" "private_assoc" {
+
+  subnet_id      = aws_subnet.private_subnet_1.id
+
+  route_table_id = aws_route_table.private_rt.id
+
+}
