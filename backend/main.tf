780b2f41cb534ccf0b747df1865b6132c39b4a36
shiva1925 <shivasai1925@gmai.com>
2026-05-13 16:05:21 +0530

    Added Terraform backend infrastructure

---
+++ backend/main.tf
@@ -0,0 +1,32 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "shiva-terraform-state-bucket-2026"
+
+  tags = {
+    Name = "Terraform State Bucket"
+  }
+}
+
+
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name         = "terraform-lock-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "Terraform Lock Table"
+  }
+}
